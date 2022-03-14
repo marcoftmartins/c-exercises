df5cd53cac62c8de885eb7b6f411a9a059487c86
marcoftmartins <marcofilipemartins21@gmail.com>
2022-03-14 22:15:55 +0000

    vectorSum added

---
+++ vectorSum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  int sum = 0;
+  int i;
+  int nums[5];
+  for (i=0;i<5;i++){
+    printf("Input a value :");
+    scanf("%d",&nums[i]);
+  }
+  for (i=0;i<5;i++){
+    sum+=nums[i];
+  }
+  printf("Soma dos valores = %d",sum);
+}
