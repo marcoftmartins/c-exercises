823da489f97ac629a4da2099f63f0929c1db44e3
marcoftmartins <marcofilipemartins21@gmail.com>
2022-03-14 22:46:35 +0000

    Added some comments

--- Arrays/vectorSum.c
+++ Arrays/vectorSum.c
@@ -2,9 +2,11 @@
 
 int main() {
   int sum = 0, i, size;
+  /* Get the array size */
   printf("Enter array size : ");
   scanf("%d",&size);
   int nums[size];
+  /* Get elements and store into  */
   for (i=0;i<size;i++){
     printf("Array value (%d) : ",i+1); 
     scanf("%d",&nums[i]);
@@ -12,5 +14,6 @@ int main() {
   for (i=0;i<size;i++){
     sum+=nums[i];
   }
+  /* Prints the sum */
   printf("Sum of the array = %d",sum);
 }
